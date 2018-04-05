165219bc9dd4671e328787356f8fd9c980feb007
Bruno Mazzo <bruno.mazzo@concrete.com.br>
2018-04-05 13:59:39 -0300

    Creating ScaleTypeApplier

---
+++ Yosef/Classes/Appliers/ScaleTypeApplier.swift
@@ -0,0 +1,16 @@
+//
+//  ScaleTypeApplier.swift
+//  Yosef
+//
+//  Created by Bruno Mazzo on 05/04/2018.
+//
+
+import Foundation
+
+class ScaleTypeApplier<ViewType: UIView>: TypedPropertyApplier {
+    
+    func apply(value: UIViewContentMode, to: ViewType) throws -> ViewType {
+        to.contentMode = value
+        return to
+    }
+}
